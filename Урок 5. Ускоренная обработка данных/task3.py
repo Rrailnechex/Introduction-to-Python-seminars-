1e57fd3bbf1f06114d9c110ef0606ed3815c0143
Ygor <wanerosu8@gmail.com .com>
2023-01-16 21:11:00 +0300

    task 3 completed

--- Урок 5. Ускоренная обработка данных/task3.py
+++ Урок 5. Ускоренная обработка данных/task3.py
@@ -18,17 +18,11 @@ def print_feeld(play_feeld):
         print()
 
 
-coordx =
-coordy
-
-""" def get_coords():
+def get_coords():
     coords = [0, 0]
-    """ coords[0], coords[1] = input("Enter two values: ").split() """
-
-    coords = [int(input("Enter 'coord x' = ")),
-              int(input("Enter 'coord y' = "))]
-
-    return coords """
+    coords = [int(input("Enter 'line' = "))-1,
+              int(input("Enter 'collum' = "))-1]
+    return coords
 
 
 def check_free_space(play_feeld):
@@ -51,13 +45,12 @@ def get_ai_coords(play_feeld):
                 free_space.append([i, j])
 
     coords = [free_space[random.randint(0, len(free_space)-1)]]
-    return coords
+    coords2 = [coords[0][0], coords[0][1]]
+    return coords2
 
 
 def insert_turn(play_feeld, coords, value):
-    a = coords[0]
-    b = coords[1]
-    play_feeld[a][b] = value
+    play_feeld[coords[0]][coords[1]] = value
     return play_feeld
 
 
@@ -69,11 +62,13 @@ def game_cycle(play_feeld):
     while loop == True:
 
         # players turn
+        print(">>> Players turn <<<")
         coords = get_coords()
         insert_turn(play_feeld, coords, 2)
         print_feeld(play_feeld)
 
         # super AI turn
+        print(">>> AIs turn <<<")
         coords = get_ai_coords(play_feeld)
         insert_turn(play_feeld, coords, 3)
         print_feeld(play_feeld)
@@ -84,7 +79,6 @@ def game_cycle(play_feeld):
 
 play_feeld = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 
-print("game started")
+print("!!!game started!!!")
 game_cycle(play_feeld)
-print_feeld(play_feeld)
-print("game started")
+print("!!!game ended!!!")
